b74c600cb709f9988b40901a0dbbf6e4ec8f4e31
Kartik <kv7709125@gmail.com>
2026-06-01 12:10:50 +0530

    CORS managed post vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://inventory-management-system-one-blush.vercel.app/"
     ],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
